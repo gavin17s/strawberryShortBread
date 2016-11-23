b2c39724cda591aa4805b22a4a0bf74ea3191af1
Gavin S <isgavinhere@users.noreply.github.com>
2016-11-23 20:52:04 +1300

    GUI calculator

---
+++ CalcGUI/src/su/gavin/example/Operation.java
@@ -0,0 +1,24 @@
+package su.gavin.example;
+
+import java.util.function.DoubleBinaryOperator;
+
+/**
+ * Created by gavinsu on 23/11/16.
+ */
+public enum Operation {
+    ADDITION((x,y) -> x+y),
+    SUBTRACTION((x,y) -> x-y),
+    DIVISION((x,y) -> x/y),
+    MULTIPLICATION((x,y) -> x*y),
+    PERCENTAGE((x,y) -> x%y);
+
+    private DoubleBinaryOperator operator;
+
+    Operation(DoubleBinaryOperator operator) {
+        this.operator = operator;
+    }
+
+    public DoubleBinaryOperator getOperator() {
+        return operator;
+    }
+}
